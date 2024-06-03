c59f4f878d2e52101344510101d5e75ee6df1f02
Ekaterina Titova <2136224@cegepmontpetit.ca>
2024-06-03 14:13:28 -0400

    FCT Zombie Index
    Ajout du contrôleur Zombie et de la vue Index associée affichant la liste des zombies. Ajout du menu de gestion avec Bootstrap

--- ZombieParty/ZombieParty/Controllers/ZombieController.cs
+++ ZombieParty/ZombieParty/Controllers/ZombieController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using ZombieParty.Models;
 
 namespace ZombieParty.Controllers
 {
@@ -6,7 +7,19 @@ namespace ZombieParty.Controllers
     {
         public IActionResult Index()
         {
+            this.ViewBag.MaListe = new List<Zombie>()
+    {
+        new Zombie(){Name= "LeChuck", Point=5, Type="Fiction",},
+        new Zombie(){Name= "Lenore", Point=4, Type="Fiction",},
+        new Zombie(){Name= "Draugr", Point=2, Type="Légendaire",},
+        new Zombie(){Name= "Ragamuffin", Point=5, Type="Légendaire",},
+        new Zombie(){Name= "Taxidermy", Point=1, Type="Légendaire",},
+        new Zombie(){Name= "chien de l'enfer", Point=7, Type="Fiction",},
+        new Zombie(){Name= "Avogadro", Point=9, Type="Fiction",}
+    };
+
             return View();
         }
+
     }
 }
